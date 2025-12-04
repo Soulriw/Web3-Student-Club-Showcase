65a4fd99654fa2628dd7a5cbfd596355d1d0cf55
PinozenTH <pinozen.thailand@gmail.com>
2025-12-04 16:59:16 +0700

    finishing up ui atom mac address handler (not finish)

--- m5paper/m5paper.ino
+++ m5paper/m5paper.ino
@@ -1,36 +1,96 @@
 #include <M5EPD.h>
 #include <WiFi.h>
 #include <HTTPClient.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <esp_now.h>
 
-// สร้าง Canvas 2 ใบ (ใบใหญ่=เมนู, ใบเล็ก=Status)
+const char *SSID_AP = "Web3Showcase_AP";
+const char *PASSWORD_AP = NULL;
+const int LOCAL_PORT = 88; 
+
+IPAddress localIP(192, 168, 4, 8);
+IPAddress gateway(192, 168, 4, 1);   
+IPAddress subnet(255, 255, 255, 0); 
+uint8_t atomMAC[] = {0x4C, 0x75, 0x25, 0xAC, 0xBE, 0x18};
+
 M5EPD_Canvas canvas(&M5.EPD);
+M5EPD_Canvas status_canvas(&M5.EPD); 
+
+AsyncWebServer paperServer(LOCAL_PORT);
+
+int selectedChoice = 0;
+
+void onDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
+  // --- TRIGGER EVENT ON M5PAPER ---
+  Serial.print("receive from matrix");
+}
+
+void sendCommand(const char *cmd) {
+  strcpy(outgoing.msg, cmd);
+  esp_now_send(atomMAC, (uint8_t *)&outgoing, sizeof(outgoing));
+  Serial.print("Sent: ");
+  Serial.println(cmd);
+}
 
 void drawMenu() {
   canvas.createCanvas(540, 960);
 
-  // CAMT WEB3 CAFE
   canvas.setTextSize(4);
   canvas.drawString("CAMT WEB3 CAFE", 100, 50);
 
   canvas.setTextSize(3);
 
-  // 1. Coffee........................2 CCoin
   canvas.drawRect(0, 130, 540, 80, 15);
-  canvas.drawString("1. Coffee..............2 CCoin", 30, 155);
+  canvas.drawString("1. Coffee......2 CCoin", 30, 155);
+  defaultSelectButton(1);
 
-  // 2. Croissant.....................3 CCoin
   canvas.drawRect(0, 230, 540, 80, 15);
-  canvas.drawString("2. Croissant...........3 CCoin", 30, 255);
+  canvas.drawString("2. Croissant...3 CCoin", 30, 255);
+  defaultSelectButton(2);
 
-  // 3. Lunch Set.....................5 CCoin
   canvas.drawRect(0, 330, 540, 80, 15);
-  canvas.drawString("3. Lunch Set...........5 CCoin", 30, 355);
+  canvas.drawString("3. Lunch Set...5 CCoin", 30, 355);
+  defaultSelectButton(3);
 
-  // 4. Tea...........................2 CCoin
   canvas.drawRect(0, 430, 540, 80, 15);
-  canvas.drawString("4. Tea.................2 CCoin", 30, 455);
+  canvas.drawString("4. Tea.........2 CCoin", 30, 455);
+  defaultSelectButton(4);
 
   canvas.pushCanvas(0,0, UPDATE_MODE_GC16);
+}
+
+void defaultSelectButton(int choice) {
+  int ySelector = choice * 100 + 70;
+  canvas.fillCircle(490, ySelector, 30, 0);
+  canvas.drawCircle(490, ySelector, 30, 15);
+}
+
+void selectButton(int choice) {
+  int ySelector = choice * 100 + 70;
+  canvas.fillCircle(490, ySelector, 30, 15);
+}
+
+void updateStatus(String msg) {
+  status_canvas.createCanvas(540, 100); 
+
+  status_canvas.fillCanvas(0);          
+  status_canvas.setTextSize(3);
+  status_canvas.drawString(msg, 20, 20);
+  
+  status_canvas.pushCanvas(0, 700, UPDATE_MODE_DU4); 
+}
+
+void sumbitStatus(String msg1, String msg2) {
+  status_canvas.createCanvas(540, 100); 
+
+  status_canvas.fillCanvas(0);          
+  status_canvas.setTextSize(3);
+  status_canvas.drawString(msg1, 20, 20);
+  status_canvas.drawString(msg2, 23, 50);
+  status_canvas.pushCanvas(0, 700, UPDATE_MODE_DU4);
+
+  Serial.println("Submitted");
 }
 
 void setup() {
@@ -40,16 +100,48 @@ void setup() {
     
   M5.EPD.SetRotation(90); 
   M5.EPD.Clear(true);
-  M5.TP.SetRotation(0); // หมุนระบบสัมผัสให้ตรงกัน
+  M5.TP.SetRotation(0);
 
-  drawMenu();     
+  // ESP-NOW init
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("ESP-NOW Init Failed!");
+    return;
+  }
 
-  Serial.print("Connecting");
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.print(".");
+  esp_now_peer_info_t peerInfo = {};
+  memcpy(peerInfo.peer_addr, atomMAC, 6);
+  peerInfo.channel = 0;
+  peerInfo.encrypt = false;
+  esp_now_add_peer(&peerInfo);
+
+  drawMenu();
+
+  WiFi.mode(WIFI_STA);
+  WiFi.config(localIP, gateway, subnet);
+  WiFi.begin(SSID_AP, PASSWORD_AP);
+
+  String mac = WiFi.macAddress();
+  Serial.print("M5Paper MAC Address: ");
+  Serial.println(mac);
+
+  while (WiFi.status() != WL_CONNECTED) { 
     delay(500);
+    Serial.print(".");
   }
   Serial.println("\nConnected!");
+
+  paperServer.on("/menu", HTTP_GET, handleGetRequest); 
+}
+
+void handleGetRequest(AsyncWebServerRequest *request) {
+  String receivedUsername = "";
+  if (request->hasParam("value", true)) { // POST
+    
+  } else if (request->hasParam("value", false)) { // GET
+
+  } else {
+    request->send(400, "text/plain", "Missing Parameter.");
+  }
 }
 
 void loop() {
@@ -60,6 +152,40 @@ void loop() {
       // อ่านค่า X (0-540) และ Y (0-960)
       int x = M5.TP.readFingerX(0);
       int y = M5.TP.readFingerY(0);
+
+      if (selectedChoice != 1 && x >= 130 && x <= 229) {
+        if (selectedChoice > 0) {
+          defaultSelectButton(selectedChoice);
+        }
+        selectedChoice = 1;
+        selectButton(selectedChoice);
+        canvas.pushCanvas(0, 0, UPDATE_MODE_DU4);
+      }
+      else if (selectedChoice != 2 && x >= 230 && x <= 329) {
+        if (selectedChoice > 0) {
+          defaultSelectButton(selectedChoice);
+        }
+        selectedChoice = 2;
+        selectButton(selectedChoice);
+        canvas.pushCanvas(0, 0, UPDATE_MODE_DU4);
+      }
+      else if (selectedChoice != 3 && x >= 330 && x <= 410) {
+        if (selectedChoice > 0) {
+          defaultSelectButton(selectedChoice);
+        }
+        selectedChoice = 3;
+        selectButton(selectedChoice);
+        canvas.pushCanvas(0, 0, UPDATE_MODE_DU4);
+      }
+      else if (selectedChoice != 4 && x >= 430 && x <= 510) {
+        if (selectedChoice > 0) {
+          defaultSelectButton(selectedChoice);
+        }
+        selectedChoice = 4;
+        selectButton(selectedChoice);
+        canvas.pushCanvas(0, 0, UPDATE_MODE_DU4);
+      }
+
       delay(100);
       x = 0;
       y = 0;
